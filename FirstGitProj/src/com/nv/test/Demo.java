7b9ef49bbd17e33c7417bc3010a79bc7b098fe21
Dev1 <Narendra@DESKTOP-RMDE9Q0>
2023-03-19 10:15:36 +0530

    Demo.java Added

---
+++ FirstGitProj/src/com/nv/test/Demo.java
@@ -0,0 +1,13 @@
+package com.nv.test;
+
+import com.nv.beans.Test;
+import com.nv.factory.Today;
+
+public class Demo {
+	public static void main(String[] args) {
+		new Test().getMessage();
+		System.out.println(new Today().getDate());
+
+	}
+
+}
